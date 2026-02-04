4f262e5123e90bc979440900e2c05d9230e0da60
ShichenLuo <56015484+ShichenLuo@users.noreply.github.com>
2026-02-04 07:16:08 +0000

    python

--- sdk/python/s3.py
+++ sdk/python/s3.py
@@ -1,5 +1,18 @@
 import os
 import boto3
 import bs4
+from botocore.exceptions import ClientError
 
-print(os.environ.get("bucket_name"))
+session = boto3.Session(profile_name="myprofile",region_name="us-east-1")
+# bucket_name = os.environ["bucket_name"]
+bucket_name = "my-example-py"
+
+s3 = session.client("s3")
+try:
+    s3.create_bucket(
+        Bucket="my-example-py"
+    )
+except ClientError as e:
+    print("ERROR:", e.response["Error"]["Code"])
+
+print(s3.list_objects(Bucket=bucket_name))
