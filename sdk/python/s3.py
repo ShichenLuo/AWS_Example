2d03ddf67c91f9b5fbce85887be1dcdb6ebbffa1
ShichenLuo <56015484+ShichenLuo@users.noreply.github.com>
2026-02-05 05:06:43 +0000

    python code

--- sdk/python/s3.py
+++ sdk/python/s3.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import bs4
+import uuid
 from botocore.exceptions import ClientError
 
 session = boto3.Session(profile_name="myprofile",region_name="us-east-1")
@@ -16,3 +17,16 @@ except ClientError as e:
     print("ERROR:", e.response["Error"]["Code"])
 
 print(s3.list_objects(Bucket=bucket_name))
+
+prefix = "uuid"
+for i in range(5):
+    filename = f"testfile_{i}.txt"
+    with open(f"testfiles/{filename}","w") as f:
+        f.write(str(uuid.uuid4()))
+        print(f"writing file {filename}")
+    with open(f"testfiles/{filename}","rb") as f:
+        s3.upload_fileobj(f, bucket_name, f"{prefix}/{filename}")
+        print(f"uploading file {filename} to s3/{bucket_name}/{prefix}/{filename}")
+
+    
+
