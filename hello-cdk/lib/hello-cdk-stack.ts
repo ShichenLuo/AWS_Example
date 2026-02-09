6fe1e537b739a647e32deb51e790793726120c70
ShichenLuo <56015484+ShichenLuo@users.noreply.github.com>
2026-02-09 06:27:04 +0000

    pulumu

--- hello-cdk/lib/hello-cdk-stack.ts
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -1,19 +1,13 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib/core';
+import { Duration, Stack, StackProps, RemovalPolicy  } from 'aws-cdk-lib/core';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'HelloCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const bucket = new s3.Bucket(this, 'my-example-cdk');
   }
 }
